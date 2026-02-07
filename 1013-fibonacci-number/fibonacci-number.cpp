b99df3b1d9d31f6bae04522620274c0ce7ab9291
Vaasu Agarwal <agarwalvaasu7@gmail.com>
2026-02-07 15:54:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (35.95%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,21 +1,48 @@
 class Solution {
 public:
 
-    int solve(int n , vector<int>& t){
+    // int solve(int n , vector<int>& t){
+    //     if(n <= 1)
+    //         return n;
+
+    //     if(t[n] != -1){
+    //         return t[n];
+    //     }
+
+    //     return t[n] = solve(n- 1, t) + solve(n-2, t);
+    //     }
+    // int fib(int n) {
+    //     if (n == 0) return 0;
+    //     if (n == 1) return 1;
+
+    //     vector<int> t(n+1, -1);
+    //     return solve(n, t);
+    // }
+
+    int fib(int n){
+        // if(n <= 1)
+        //     return n;
+
+        // vector<int> t(n+1, -1);
+        // t[0] = 0;
+        // t[1] =1;
+
+        // for(int i = 2; i <= n; i++){
+        //     t[i] = t[i-1] + t[i-2];
+        // }
+        // return t[n];
+
         if(n <= 1)
             return n;
+        int c;
+        int a = 0;
+        int b =1;
 
-        if(t[n] != -1){
-            return t[n];
+        for(int i = 1; i <n ; i++){
+            c = a+b;
+            a = b;
+            b = c;
         }
-
-        return t[n] = solve(n- 1, t) + solve(n-2, t);
-        }
-    int fib(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-
-        vector<int> t(n+1, -1);
-        return solve(n, t);
+        return c;
     }
 };
