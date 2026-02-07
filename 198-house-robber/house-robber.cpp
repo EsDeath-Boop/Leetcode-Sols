90f1ece57ae3d2481bda18c2f2de8189a8920484
Vaasu Agarwal <agarwalvaasu7@gmail.com>
2026-02-07 21:50:44 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (21.51%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
-    int t[101];
+    // int t[101];
 
-    int solve(vector<int>& nums, int i, int n){
-        if(i >= n) return 0;
+    // int solve(vector<int>& nums, int i, int n){
+    //     if(i >= n) return 0;
 
-        if(t[i] != -1){
-            return t[i];
-        }
+    //     if(t[i] != -1){
+    //         return t[i];
+    //     }
 
-        int skip = solve(nums, i+1, n);
-        int select =nums[i] + solve(nums, i+2, n);
+    //     int skip = solve(nums, i+1, n);
+    //     int select =nums[i] + solve(nums, i+2, n);
 
-        return t[i] =  max(skip, select);
-    }
-    int rob(vector<int>& nums) {
+    //     return t[i] =  max(skip, select);
+    // }
+    // int rob(vector<int>& nums) {
+    //     int n = nums.size();
+    //     memset(t, -1, sizeof(t));
+    //     return solve(nums, 0, n);
+    // }
+
+    int rob(vector<int>& nums){
         int n = nums.size();
-        memset(t, -1, sizeof(t));
-        return solve(nums, 0, n);
+
+        vector<int> t(n+1, 0);
+
+        t[0] = 0;
+        t[1] = nums[0];
+        for(int i = 2; i <= n; i++){
+            int steal = nums[i-1] + t[i-2];
+            int skip = t[i-1];
+
+            t[i] = max(skip, steal);
+        }
+        return t[n];
     }
 };
