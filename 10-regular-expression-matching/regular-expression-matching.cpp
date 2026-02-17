e4470b7c3ecdb59d311ab1bd75160ad1e87507d2
Vaasu Agarwal <agarwalvaasu7@gmail.com>
2026-02-17 15:23:11 +0530

    Time: 18 ms (14.55%) | Memory: 22.9 MB (5.02%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-    bool solve(string s, string p){
-        if(p.length() == 0){
-            return (s.length() == 0);
+    
+    bool solve(int i, int j, string s, string p, vector<vector<int>>& t){
+        if(p.length() == j){
+            return (s.length() == i);
         }
+
         bool firstMatched = false;
 
-        if(s.length() > 0 && (p[0] == s[0] || p[0] == '.')){
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+
+        if(i < s.length() && (p[j] == s[i] || p[j] == '.')){
             firstMatched = true;
         }
-        if(p[1] == '*'){
-            bool notTake = solve(s, p.substr(2));
-            bool take = (firstMatched && solve(s.substr(1), p));
+        if(j+1 < p.length() && p[j+1] == '*'){
+            bool notTake = solve(i, j+2, s, p, t);
+            bool take = (firstMatched && solve(i+1, j, s, p, t));
 
-            return notTake || take;
+            return t[i][j] = notTake || take;
         }
         
-         return firstMatched && solve(s.substr(1), p.substr(1));
+         return t[i][j] = firstMatched && solve(i+1, j+1, s, p, t);
     }
     bool isMatch(string s, string p) {
-        return solve(s, p);
+        vector<vector<int>> t(21, vector<int> (21, -1));
+        return solve(0, 0, s, p, t);
     }
 };
