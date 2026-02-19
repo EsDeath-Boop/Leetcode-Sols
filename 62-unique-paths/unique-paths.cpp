e170867b38e5329c6ac1e42911c9db5bf84f12e2
Vaasu Agarwal <agarwalvaasu7@gmail.com>
2026-02-19 13:59:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (67.04%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
-    int solve(int r, int c, int m, int n, vector<vector<int>>& t){
-        if(r == m -1 && c == n - 1)
-            return 1;
-        if(t[r][c] != -1)
-            return t[r][c];
+
+    // memoization
+
+    // int solve(int r, int c, int m, int n, vector<vector<int>>& t){
+    //     if(r == m -1 && c == n - 1)
+    //         return 1;
+    //     if(t[r][c] != -1)
+    //         return t[r][c];
         
-        int down = 0;
-        int right = 0;
-        if(r < m - 1){
-            down = solve(r + 1, c, m, n, t);
-        }
-        if(c < n -1){
-            right = solve(r, c+1, m, n, t);
-        }
-        return t[r][c] =  down + right;
-    }
+    //     int down = 0;
+    //     int right = 0;
+
+    //     int prev1 = 0;
+    //     int prev2
+    //     if(r < m - 1){
+    //         down = solve(r + 1, c, m, n, t);
+    //     }
+    //     if(c < n -1){
+    //         right = solve(r, c+1, m, n, t);
+    //     }
+    //     return t[r][c] =  down + right;
+    // }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> t(m+1, vector<int>(n+1, -1));
-        return solve(0, 0, m, n, t);
+        vector<vector<int>> t(m+1, vector<int>(n+1, 0));
+        // return solve(0, 0, m, n, t);
+        // t[0][0] = 0;
+        for(int i = 0; i < m; i++)
+            t[i][0] = 1;
+
+        for(int i =0;i < n; i++){
+            t[0][i] = 1;
+        }
+
+        for(int i = 1;i < m;i++){
+            for(int j =1; j < n; j++){
+                t[i][j] = t[i-1][j] + t[i][j-1];
+            }
+        }
+        return t[m-1][n-1];
     }
 };
