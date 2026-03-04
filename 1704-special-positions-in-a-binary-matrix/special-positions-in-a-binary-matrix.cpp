0a4b07578aa0c1f525c8c7d5e39e48c80177a5da
Vaasu Agarwal <agarwalvaasu7@gmail.com>
2026-03-04 21:15:38 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (20.57%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -1,42 +1,72 @@
+// class Solution {
+// public:
+//     int check(vector<vector<int>>& mat, int m, int n, int row, int col){
+//         int rowCount = 0, colCount = 0;
+
+//         for(int j = 0; j < n; j++){
+//             if(mat[row][j] == 1)
+//                 rowCount++;
+//         }
+
+//         for(int i = 0; i < m; i++){
+//             if(mat[i][col] == 1)
+//                 colCount++;
+//         }
+
+//         if(rowCount == 1 && colCount == 1)
+//             return 1;
+
+//         return 0;
+//     }
+
+//     int solve(int m, int n, vector<vector<int>>& mat){
+//         int count = 0;
+
+//         for(int i = 0; i < m; i++){
+//             for(int j = 0; j < n; j++){
+//                 if(mat[i][j] == 1 && check(mat, m, n, i, j)){
+//                     count++;
+//                 }
+//             }
+//         }
+
+//         return count;
+//     }
+
+//     int numSpecial(vector<vector<int>>& mat) {
+//         int m = mat.size();
+//         int n = mat[0].size();
+
+//         return solve(m, n, mat);
+//     }
+// };
+
 class Solution {
 public:
-    int check(vector<vector<int>>& mat, int m, int n, int row, int col){
-        int rowCount = 0, colCount = 0;
-
-        for(int j = 0; j < n; j++){
-            if(mat[row][j] == 1)
-                rowCount++;
-        }
-
-        for(int i = 0; i < m; i++){
-            if(mat[i][col] == 1)
-                colCount++;
-        }
-
-        if(rowCount == 1 && colCount == 1)
-            return 1;
-
-        return 0;
-    }
-
-    int solve(int m, int n, vector<vector<int>>& mat){
-        int count = 0;
-
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(mat[i][j] == 1 && check(mat, m, n, i, j)){
-                    count++;
-                }
-            }
-        }
-
-        return count;
-    }
-
     int numSpecial(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
 
-        return solve(m, n, mat);
+        vector<int> row(m,0), col(n,0);
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j] == 1){
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+
+        int ans = 0;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j] == 1 && row[i] == 1 && col[j] == 1)
+                    ans++;
+            }
+        }
+
+        return ans;
     }
 };
