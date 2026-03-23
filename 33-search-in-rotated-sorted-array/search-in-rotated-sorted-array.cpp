3842ed9f25d746b7e5d0eae23eca546c5b7dd549
Vaasu Agarwal <agarwalvaasu7@gmail.com>
2026-03-23 19:33:03 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (34.77%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& nums, int target, int low, int high){
-        while(low <= high){
+    int binarySearch(vector<int>& nums, int target, int low, int high) {
+        while (low <= high) {
             int mid = low + (high - low) / 2;
 
-            if(nums[mid] == target)
+            if (nums[mid] == target)
                 return mid;
-            else if(nums[mid] < target)
+            else if (nums[mid] < target)
                 low = mid + 1;
             else
                 high = mid - 1;
@@ -14,19 +14,28 @@ public:
         return -1;
     }
 
-    int findPivot(vector<int>& nums){
+    int findPivot(vector<int>& nums) {
         int n = nums.size();
         int low = 0, high = n - 1;
 
-        while(low < high){
+        while (low <= high) {
+            if(nums[low] <= nums[high])
+                return low;
+                
             int mid = low + (high - low) / 2;
 
-            if(nums[mid] > nums[high])
+            int prev = (mid - 1 + n) % n;
+            int next = (mid + 1) % n;
+
+            if (nums[mid] <= nums[prev] && nums[mid] <= nums[next])
+                return mid;
+
+            if (nums[mid] >= nums[low])
                 low = mid + 1;
             else
-                high = mid;
+                high = mid - 1;
         }
-        return low; // pivot index
+        return 0;
     }
 
     int search(vector<int>& nums, int target) {
@@ -35,7 +44,8 @@ public:
 
         // search in left half
         int res = binarySearch(nums, target, 0, pivot - 1);
-        if(res != -1) return res;
+        if (res != -1)
+            return res;
 
         // search in right half
         return binarySearch(nums, target, pivot, n - 1);
