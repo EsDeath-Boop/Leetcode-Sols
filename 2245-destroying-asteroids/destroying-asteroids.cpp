4650b30de409258637d1e9fdb545c66bd7fe7302
Vaasu Agarwal <agarwalvaasu7@gmail.com>
2026-05-31 12:12:33 +0530

    Time: 0 ms (100.00%) | Memory: 106.9 MB (16.20%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.cpp
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
-    bool asteroidsDestroyed(long long mass, vector<int>& arr) {
-        sort(arr.begin(), arr.end());
+    bool asteroidsDestroyed(long long mass, vector<int>& asteroids) {
+        int n=asteroids.size();
+        
+        int count = 0;
 
-        for(auto &x : arr){
-            if(mass < x)
-                return false;
-
-            mass += x;
+        for(int i=0;i<n;i++){
+            if(mass>=asteroids[i]){
+                mass+=asteroids[i];
+                asteroids[i]=0;
+            }
+            else count+=1;
         }
+
+        while(count>0){
+            int temp=0;
+            for(int i=0;i<n;i++){
+                if(mass>=asteroids[i]){
+                    mass+=asteroids[i];
+                    asteroids[i]=0;
+                }
+
+                else temp+=1;
+            }
+
+            if(count==temp) return false;
+            count=temp;
+        }
+
         return true;
     }
 };
