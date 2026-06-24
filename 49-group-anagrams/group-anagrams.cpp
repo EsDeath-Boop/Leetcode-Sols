94e96ac40e1b3835689bf643704f27e042371e86
Vaasu Agarwal <agarwalvaasu7@gmail.com>
2026-06-24 19:10:11 +0530

    Time: 23 ms (30.29%) | Memory: 29.6 MB (19.03%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
+    string solve(string &word){
+        vector<int> arr(26, 0);
+
+        for(char &c : word){
+            arr[c - 'a']++;
+        }
+
+        string newWord = "";
+        for(int i = 0; i < 26; i++){
+            int freq = arr[i];
+
+            if(freq > 0){
+                newWord += string(freq, i + 'a');
+            }
+        }
+        return newWord;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& s) {
         int n = s.size();
+
         vector<vector<string>> res;
 
         unordered_map<string, vector<string>> mp;
 
         for(int i = 0; i < n; i++){
             string temp = s[i];
-            sort(temp.begin(), temp.end());
-            mp[temp].push_back(s[i]);
+
+            string newWord = solve(temp);
+
+            mp[newWord].push_back(temp);
         }
 
-        for(auto it : mp){
+        for(auto &it : mp){
             res.push_back(it.second);
         }
         return res;
