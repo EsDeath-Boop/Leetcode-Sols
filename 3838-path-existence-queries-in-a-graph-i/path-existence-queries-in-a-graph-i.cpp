9e6bc4117eb386340d0bba94664cf7220efe5bae
Vaasu Agarwal <agarwalvaasu7@gmail.com>
2026-07-10 00:37:07 +0530

    Time: 4 ms (93.70%) | Memory: 224.1 MB (78.36%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -1,40 +1,61 @@
 class Solution {
 public:
-    void dfs(int u, vector<vector<int>>& adj, vector<int>& comp, int compId){
-        comp[u] = compId;
+    // void dfs(int u, vector<vector<int>>& adj, vector<int>& comp, int compId){
+    //     comp[u] = compId;
 
-        for(auto &child : adj[u]){
-            if(comp[child] == -1){
-                dfs(child, adj, comp, compId);
-            }
-        } 
-    }
+    //     for(auto &child : adj[u]){
+    //         if(comp[child] == -1){
+    //             dfs(child, adj, comp, compId);
+    //         }
+    //     } 
+    // }
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        vector<vector<int>> adj(n);
+    //     vector<vector<int>> adj(n);
 
-        for(int i = 0; i < n-1; i++){
-            if(nums[i+1] - nums[i] <= maxDiff){
-                adj[i].push_back(i+1);
-                adj[i+1].push_back(i);
-            }
-        }
+    //     for(int i = 0; i < n-1; i++){
+    //         if(nums[i+1] - nums[i] <= maxDiff){
+    //             adj[i].push_back(i+1);
+    //             adj[i+1].push_back(i);
+    //         }
+    //     }
+
+    //     vector<int> comp(n, -1);
+    //     int compId = 0;
+
+    //     for(int i = 0; i < n; i++){
+    //         if(comp[i] == -1){
+    //             dfs(i, adj, comp, compId);
+    //             compId++;
+    //         }
+    //     }
+    //     vector<bool> res;
+
+    //     for(auto &q : queries){
+    //         if(comp[q[0]] == comp[q[1]]){
+    //             res.push_back(1);
+    //         }else
+    //             res.push_back(0);
+    //     }
+    //     return res;
 
         vector<int> comp(n, -1);
+        vector<bool> res;
         int compId = 0;
-
-        for(int i = 0; i < n; i++){
-            if(comp[i] == -1){
-                dfs(i, adj, comp, compId);
+        comp[0] = compId;
+        for(int i = 1; i < n; i++){
+            if(nums[i] - nums[i-1] > maxDiff){
                 compId++;
             }
+            comp[i] = compId;
+            
         }
-        vector<bool> res;
 
         for(auto &q : queries){
             if(comp[q[0]] == comp[q[1]]){
                 res.push_back(1);
-            }else
+            }else{
                 res.push_back(0);
+            }
         }
         return res;
     }
